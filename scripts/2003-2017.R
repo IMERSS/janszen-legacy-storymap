60ded3dc17080ba9a2a3bb88130b20d86fcdbc01
chlorophilia <andrewsimon@MacBook-Air.lan>
2023-02-25 10:19:18 -0800

    added plotly figures summarizing the number of records digitized for each of Harvey's journals

--- scripts/2003-2017.R
+++ scripts/2003-2017.R
@@ -41,3 +41,55 @@ Map <- leaflet() %>%
   fitBounds(bbox[1], bbox[2], bbox[3], bbox[4])
 
 print(Map)
+
+
+# Create pie chart summarizing no. of records digitized by research collection (RBCM, UBC)
+
+RBCM.records <- read.csv("tabular_data/HJ_RBCM_Records_2021-11-15.csv")
+UBC.records <- read.csv("tabular_data/HJ_UBC_records_2023-02-12.csv")
+
+# First remove non-numeric characters from collection numbers
+
+RBCM.records$CollectorsFieldNumber <- gsub("[^0-9.]", "", RBCM.records$CollectorsFieldNumber)
+RBCM.records$CollectorsFieldNumber <- as.numeric(RBCM.records$CollectorsFieldNumber)
+
+UBC.records$Collector.Number <- gsub("[^0-9.]", "", UBC.records$Collector.Number)
+UBC.records$Collector.Number <- as.numeric(UBC.records$Collector.Number)
+
+# Remove records not attributable to Harvey from UBC records
+
+UBC.records <- UBC.records %>% dplyr::filter(str_detect(Primary.Collector, 'Harvey Janszen'))
+
+# Select records having collection numbers in the range of 1:1929 (collections from this time period, as per field notes)
+
+RBCM.records.1973_1981 <- subset(RBCM.records, RBCM.records$CollectorsFieldNumber %in% 2962:2971)
+UBC.records.1973_1981 <- subset(UBC.records, UBC.records$Collector.Number %in% 2962:2971)
+
+# Sum specimens accessioned for the time period and infer number of records that remain undigitized
+
+RBCM.HJ <- nrow(RBCM.records.1973_1981) 
+UBC.HJ <- nrow(UBC.records.1973_1981)
+undigitized <- 9-(RBCM.HJ+UBC.HJ)
+
+# Create Plotly Donut Chart
+
+Collection <- c("RBCM", "UBC", "Undigitized")
+Records <- c(RBCM.HJ, UBC.HJ, undigitized)
+
+colors <-  c("#ffcf20FF", "#10a53dFF", "#541352FF")
+
+digitized.summary <- data.frame(Collection,Records)
+
+digitized.plot <- digitized.summary %>% plot_ly(labels = ~Collection, values = ~Records, 
+                                                textposition = 'inside',
+                                                textinfo = 'label+percent',
+                                                insidetextfont = list(color = '#FFFFFF'),
+                                                hoverinfo = 'text',
+                                                marker = list(colors = colors,
+                                                              line = list(color = '#FFFFFF', width = 1)), showlegend = T)
+digitized.plot <- digitized.plot %>% add_pie(hole = 0.6)
+digitized.plot <- digitized.plot %>% layout(title = "Records digitized by collection",  showlegend = T,
+                                            xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                                            yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+
+digitized.plot
