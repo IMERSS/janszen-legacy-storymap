89e7fa370112f218eab03995cff6f59d247af170
chlorophilia <andrewsimon@MacBook-Air.local>
2023-03-01 00:49:19 -0800

    attempted to merge bounding boxes for two vectors in order to redefine broader bounding box for all shapes, but failed

--- scripts/2003-2017.R
+++ scripts/2003-2017.R
@@ -22,6 +22,14 @@ points.2003_2017$Y <- points.coordinates$Y
 
 bbox <- st_bbox(polygons.2003_2017) %>% as.vector()
 
+#bbox1 <- st_as_sfc(st_bbox(polygons.2003_2017))
+#bbox2 <- st_as_sfc(st_bbox(points.2003_2017))
+
+#bbox3 <-  c(bbox1, bbox2)
+
+#bbox <- st_bbox(bbox3)
+
+
 # Render leaflet map
 
 Map <- leaflet() %>%
