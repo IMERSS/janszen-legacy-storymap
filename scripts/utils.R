e32fc4926fd1c3eb138900065fc8782327f07fed
Antranig Basman <antranig.basman@colorado.edu>
2023-06-09 14:47:53 +0100

    Reknitted with Pam's additional paragraph, file size is now reduced through not inlining images and rounding coordinates

--- scripts/utils.R
+++ scripts/utils.R
@@ -1,9 +1,53 @@
+library(sf)
+
 lat_lon <- function (data) {
-  return(st_transform(data, "+proj=longlat +datum=WGS84"))
+  return (st_transform(data, "+proj=longlat +datum=WGS84"))
 }
 
-mx_read <- function (filename) {
-   st_data <- st_read(filename, quiet=TRUE);
-   dropped <- st_zm(st_data, drop = T, what = "ZM")
-   return(lat_lon(dropped));
+roundmulti <- function (multi, digits) {
+  multi <- lapply(multi, function (matrix) {
+    matrix <- lapply(matrix, function (coords) {
+      round(coords, digits)
+    })
+  })
+  return (st_multipolygon(multi))
+}
+
+roundpoly <- function (poly, digits) {
+  poly <- lapply(poly, function (matrix) {
+      round(matrix, digits)
+  })
+  return (st_polygon(poly))
+}
+
+round_sf <- function (fc, digits) {
+  # https://gis.stackexchange.com/questions/329110/removing-empty-polygon-from-sf-object-in-r
+  simple  <- fc
+  # TODO: Should read   simple  <- fc %>% st_simplify(preserveTopology = TRUE, dTolerance = 5) %>% filter(!st_is_empty(.)) but filter fails
+  geom <- simple$geometry
+  geom <- lapply(geom, function (one) {
+    if (inherits(one, "MULTIPOLYGON")) {
+      one <- roundmulti(one, digits)
+    } else if (inherits(one, "POLYGON")) {
+      one <- roundpoly(one, digits)
+    } else if (inherits(one, "XY")) {
+      one <- round(one) 
+    } else if (!st_is_empty(one)) {
+      stop(paste("I don't know what it is ", class(one)))
+    }
+  })
+  simple$geometry <- st_sfc(geom)
+  simple
+}
+
+mx_read <- function (filename, digits = 4) {
+  st_data <- st_read(filename, quiet=TRUE);
+  dropped <- st_zm(st_data, drop = T, what = "ZM")
+  trans <- lat_lon(dropped);
+  rounded <- round_sf(trans, digits);
+}
+
+# Attach the region's label as an "mx_regionId" option in the output data
+labelToOption <- function (label) {
+  return (list(mx_regionId = label))
 }
